eb5b0be74fae8d81d1753477a504cb29ef44afb6
TheM4HD1 <mahdimakhdomi@outlook.com>
2019-04-20 15:06:00 +0430

    Initial Commit

---
+++ Siwa/Siwa.h
@@ -0,0 +1,19 @@
+//
+//  Siwa.h
+//  Siwa
+//
+//  Created by Mahdi on 4/20/19.
+//  Copyright © 2019 Mahdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Siwa.
+FOUNDATION_EXPORT double SiwaVersionNumber;
+
+//! Project version string for Siwa.
+FOUNDATION_EXPORT const unsigned char SiwaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Siwa/PublicHeader.h>
+
+
