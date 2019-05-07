4a6b5c428f4049bb3d07a4903dbad378dc4649f4
TheM4HD1 <mahdimakhdomi@outlook.com>
2019-05-08 03:32:21 +0430

    URLs updated.

--- Siwa/API/Constants/URLs.swift
+++ Siwa/API/Constants/URLs.swift
@@ -13,10 +13,28 @@ struct URLs {
 
     }
     
-    private static let instagram = "https://instagram.com"
-    private static let login = "/accounts/login/ajax/"
+    private static let instagram = "https://www.instagram.com"
+    private static let loginPath = "/accounts/login/ajax/"
+    private static let twoFactorPath = "/accounts/login/ajax/two_factor/"
+    private static let twoFactorResendPath = "/accounts/send_two_factor_login_sms/"
 
-    static func getLogin() -> URL {
-        return URL(string: String(format: "%@%@", instagram, login))!
+    static func home() -> URL {
+        return URL(string: instagram)!
+    }
+    
+    static func login() -> URL {
+        return URL(string: String(format: "%@%@", instagram, loginPath))!
+    }
+    
+    static func checkpoint(url: String) -> URL {
+        return URL(string: String(format: "%@%@", instagram, url))!
+    }
+    
+    static func twoFactor() -> URL {
+        return URL(string: String(format: "%@%@", instagram, twoFactorPath))!
+    }
+    
+    static func resendTwoFactorCode() -> URL {
+        return URL(string: String(format: "%@%@", instagram, twoFactorResendPath))!
     }
 }
